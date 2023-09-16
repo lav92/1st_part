d1fe51279737f726c2996a7067544078e090a2db
lav92 <xxkukucxx@gmail.com>
2023-09-16 15:37:37 +0300

    15 lesson

--- news/views.py
+++ news/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     all_news = News.objects.order_by('-created_at')
-    return render(request, 'news/index.html', {'news': all_news, 'title': 'News'})
+    categories = Category.objects.all()
+    context = {
+        'news': all_news,
+        'title': 'News',
+        'categories': categories,
+    }
+    return render(request, 'news/index.html', context)
+
+
+def category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request, 'news/category.html', context=context)
