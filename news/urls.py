bb0e420b4eed0f3488fe325bd523826b70e61d5f
lav92 <xxkukucxx@gmail.com>
2023-09-23 19:13:06 +0300

    27 lesson views home page through Class(not method)

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', category, name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
     path('news/add_news/', add_news, name='add_news'),
--- news/views.py
+++ news/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
+
 from .models import News, Category
 from .forms import NewsForm
+
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home page'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True).order_by('-created_at')
 
 
 def index(request):
